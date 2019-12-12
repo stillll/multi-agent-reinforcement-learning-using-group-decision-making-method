43be6408b6097367e24dbb0c5f2d1d5e5c5d315d
zhj000 <46739857+zhj000@users.noreply.github.com>
2019-12-12 19:17:58 +0800

    new files
    add test and train files for different use of our model

---
+++ test.py
@@ -0,0 +1,39 @@
+
+
+def test_model(env, model, max_episode):
+
+    cumulate_reward = 0
+    for episode in range(max_episode):
+        print("test_episode", episode)
+        env_s = env.reset()  # init env and return env state
+        #print(env_s)
+        counter = 0  # if end episode
+        while True:  # one step
+            # fresh env
+            env.render()
+
+            # all agents join actions for this step
+            join_act = model.run_model(env_s)[0]
+            # take action and get next env state and reward
+            env_s_, reward, done = env.step(join_act)
+
+            cumulate_reward = reward + cumulate_reward * 0.99
+
+            counter += 1
+            if counter > 300:
+                break
+
+            env_s = env_s_
+
+            # break while loop when end of this episode
+            if done:
+                break
+
+        print("reward:", cumulate_reward)
+        model.reward_his.append(cumulate_reward)
+
+    # end of game
+    print('game over')
+    env.destroy()
+    model.plot_reward()
+
