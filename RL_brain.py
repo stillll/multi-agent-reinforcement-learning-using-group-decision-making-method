8db7c00e771e34ef00082fc614278817d2db1696
wqt-zst <981494130@qq.com>
2020-02-03 11:33:09 +0800

    add: delete 99% of episode print

--- RL_brain.py
+++ RL_brain.py
@@ -156,7 +156,7 @@ class DeepQNetwork:
         # check to replace target parameters
         if self.learn_step_counter % self.replace_target_iter == 0:
             self.sess.run(self.replace_target_op)
-            print('\ntarget_params_replaced\n')
+            #print('\ntarget_params_replaced\n')
 
         # sample batch memory from all memory
         if self.memory_counter > self.memory_size:
